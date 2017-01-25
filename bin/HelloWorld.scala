d8f884d8aea281ed7ca7ee2e5efa044e102f8604
coderrick <derrick.duncan@temple.edu>
2017-01-25 18:36:52 -0500

    update

---
+++ bin/HelloWorld.scala
@@ -0,0 +1,22 @@
+/*
+VARIABLE
+val or val VariableName : DataType = [Initial Value]
+
+MULTIPLE VARIABLES
+val (myVar1: Int, myVar2: String) = Pair(40, "Foo")
+
+FUNCTION
+def functionName ([list of parameters]) : [return type] = {
+   function body
+   return [expr]
+}
+*/
+object HelloWorld{
+  def main(args: Array[String]){
+    println("Hello World");
+  }
+}
+/*
+Quill vs Slick
+You don't need to make a really complex datastructure in Quill you just specify a bunch of cases'
+*/
